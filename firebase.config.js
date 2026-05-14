3d01211dd0704796091c8daecd1365d6e3d7d383
datnekhihi <105278530+datnekhihi@users.noreply.github.com>
2026-05-15 03:22:05 +0700

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBuwC...", // Giữ nguyên key của Đạt
+    apiKey: "AIzaSyBuwC_mqQOu0-5NSHLIJ5wq11dB-2kIop8",
     authDomain: "quanly-428c3.firebaseapp.com",
     databaseURL: "https://quanly-428c3-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "quanly-428c3",
-    storageBucket: "quanly-428c3.appspot.com",
+    storageBucket: "quanly-428c3.firebasestorage.app",
     messagingSenderId: "1061659403130",
     appId: "1:1061659403130:web:1a2a23280aae1de0d3ecc3"
 };
